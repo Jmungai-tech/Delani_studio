1e496894351e7b2ff850f7e3622460c08c0e1ef3
jermy.mthangani <jermy.mthangani@gmail.com>
2020-02-23 23:12:41 +0300

    Added click and toggle jQuery functions

---
+++ js/scripts.js
@@ -0,0 +1,13 @@
+$(function(){
+
+    $('.togle').click(function(){
+      $('.togle').toggle();
+    });
+  
+    $('.togle1').click(function(){
+      $('.togle1').toggle();
+    });
+  
+    $('.togle2').click(function(){
+      $('.togle2').toggle();
+    });
