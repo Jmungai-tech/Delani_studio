c6a68e7e4b629ea9f6712b24f49d5a289cfff652
jermy.mthangani <jermy.mthangani@gmail.com>
2020-02-23 23:14:30 +0300

    linked hover ids with the html file using jQuery

--- js/scripts.js
+++ js/scripts.js
@@ -11,3 +11,52 @@ $(function(){
     $('.togle2').click(function(){
       $('.togle2').toggle();
     });
+
+    $('#hov1').hover(function(){
+        $('#hover1').show();
+      }, function() {
+        $('#hover1').hide()
+       });
+    
+       $('#hov2').hover(function(){
+        $('#hover2').show();
+      }, function() {
+        $('#hover2').hide()
+       });
+    
+       $('#hov3').hover(function(){
+        $('#hover3').show();
+      }, function() {
+        $('#hover3').hide()
+       });
+    
+       $('#hov4').hover(function(){
+        $('#hover4').show();
+      }, function() {
+        $('#hover4').hide()
+       });
+    
+       $('#hov5').hover(function(){
+        $('#hover5').show();
+      }, function() {
+        $('#hover5').hide()
+       });
+    
+       $('#hov6').hover(function(){
+        $('#hover6').show();
+      }, function() {
+        $('#hover6').hide()
+       });
+    
+       $('#hov7').hover(function(){
+        $('#hover7').show();
+      }, function() {
+        $('#hover7').hide()
+       });
+    
+       $('#hov8').hover(function(){
+        $('#hover8').show();
+      }, function() {
+        $('#hover8').hide()
+       });
+    });
